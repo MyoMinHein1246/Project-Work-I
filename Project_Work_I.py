9dae831fd39b2a6375146475edfaaf7d7d92bbc0
Freddy <myominhein1246@gmail.com>
2023-02-07 12:23:42 +0630

    Project work I using SQL database

--- Project_Work_I.py
+++ Project_Work_I.py
@@ -1,15 +1,15 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-import sys
+import sqlite3
 
 
-FILE_NAME = "database.txt"
-DATA_HEADERS = ["Name", "Job", "Salary", "Address"]
+DATA_HEADERS = ["Id", "First name", "Last name", "Job", "Salary", "Address"]
 
 class EmployeeSearchForm(QMainWindow):
-	def __init__(self):
+	def __init__(self, cursor):
 		super(EmployeeSearchForm, self).__init__()
 		uic.loadUi("EmployeeSearchForm.ui", self)
+		self.cursor = cursor
 		self.editEmployeeName.returnPressed.connect(lambda: self.search_employee(self.editEmployeeName.text()))
 		self.btnSearch.clicked.connect(lambda: self.search_employee(self.editEmployeeName.text()))
 
@@ -37,7 +37,9 @@ class EmployeeSearchForm(QMainWindow):
 		database = self.get_employee_database()
 
 		for data in database:
-			if data[0].lower() == employeeName.lower():
+			print(data)
+			name = data[1] + " " + data[2]
+			if name.lower().strip() == employeeName.lower():
 				return data
 
 		return None
@@ -46,28 +48,44 @@ class EmployeeSearchForm(QMainWindow):
 		database = []
 
 		try:
-			with open(FILE_NAME, 'r') as f:
-				for line in f.readlines():
-					data_list = []
-					for data in line.split(','):
-						data_list.append(data.strip())
+			self.cursor.execute('SELECT * FROM employee;')
+			results = self.cursor.fetchall()
+			for line in results:
+				data_list = []
+				for data in line:
+					data_list.append(data)
 					
-					database.append(data_list)
-		except OSError:
+				database.append(data_list)
+		except sqlite3.Error as e:
 			print("Failed to read database")
+			print("[Error] ", e)
 
 		return database
 		
 def main():
-	app = QApplication.instance()
-	if not app:
-		app = QApplication([])
+	try:
+		connection = sqlite3.connect('EmployeeData.db')
+		cursor = connection.cursor()
 
-	searchForm = EmployeeSearchForm()
-	searchForm.show()
+		app = QApplication.instance()
+		if not app:
+			app = QApplication([])
 
-	sys.exit(app.exec_())
+		searchForm = EmployeeSearchForm(cursor)
+		searchForm.show()
 
+		app.exec_()
+	except sqlite3.Error as err:
+		print("Failed to connect to sqlite database.")
+		print("[Error] ", err)
+	finally:
+		if cursor:
+			cursor.close()
+			print("Closed cursor")
+		if connection:
+			connection.close()
+			print("Closed cursor")
+		
 
 if __name__ == "__main__":
 	main()
